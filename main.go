038113b8eeb100d87168809ae9550f41d8e1028f
Reza Ghanbari <ghanbari.reza94@gmail.com>
2018-08-16 16:14:11 +0430

    part 2, add goroutine

--- main.go
+++ main.go
@@ -6,35 +6,60 @@ import (
 	"fmt"
 	"encoding/csv"
 	"strings"
+	"time"
 )
 
 func main() {
 	csvFileName := flag.String("csv", "problems.csv",
-		"A csv file in the format of question/answer")
+		"A csv file in the format of `question,answer`")
+	timeLimit := flag.Int("limit", 30, "The time limit for the quiz in seconds.")
 	flag.Parse()
+
 	file, err := os.Open(*csvFileName)
 	if err != nil {
-		exit(fmt.Sprintf("Failed to open the csv file: %s\n", *csvFileName))
+		exit(fmt.Sprintf("Failed to open the CSV file: %s\n", *csvFileName))
 	}
 	r := csv.NewReader(file)
 	lines, err := r.ReadAll()
 	if err != nil {
 		exit("Failed to parse the provided CSV file.")
 	}
-
 	problems := parseLines(lines)
 
+	timer := time.NewTimer(time.Duration(*timeLimit) * time.Second)
 	correct := 0
 
+problemloop:
 	for i, p := range problems {
-		fmt.Printf("Problem #%d: %s = \n", i+1, p.q)
-		var answer string
-		fmt.Scanf("%s\n", &answer)
-		if answer == p.a {
-			correct++
+		fmt.Printf("Problem #%d: %s = ", i+1, p.q)
+		answerCh := make(chan string)
+		go func() {
+			var answer string
+			fmt.Scanf("%s\n", &answer)
+			answerCh <- answer
+		}()
+
+		select {
+		case <- timer.C:
+			fmt.Println()
+			break problemloop
+		case answer := <-answerCh:
+			if answer == p.a {
+				correct++
+			}
 		}
 	}
 	fmt.Printf("You scored %d out of %d.\n", correct, len(problems))
+}
+
+func exit(msg string) {
+	fmt.Println(msg)
+	os.Exit(1)
+}
+
+type problem struct {
+	q string
+	a string
 }
 
 func parseLines(lines [][]string) []problem {
@@ -46,14 +71,4 @@ func parseLines(lines [][]string) []problem {
 		}
 	}
 	return ret
-}
-
-type problem struct {
-	q string
-	a string
-}
-
-func exit(msg string) {
-	fmt.Printf(msg)
-	os.Exit(1)
 }
